5b4543f5abb7757c14eb2395aab766a83334bcaf
kylemao <kylemao@tencent.com>
2021-10-31 23:36:32 +0800

    modify

--- cmd/second/main.go
+++ cmd/second/main.go
@@ -5,41 +5,72 @@ import (
 	"database/sql"
 	"fmt"
 	"github.com/pkg/errors"
+	"math/rand"
+	"time"
 )
 
 const SEPARATOR_LINE = "-----------"
 
-func fn1() error {
-	err := fn2()
-	// 非根因处使用WithMessage，无需生成堆栈信息
-	return errors.WithMessage(err, "fn2")
-}
+/// 用来解耦第3方库
+var NotFound = errors.New("not found")
 
-func fn2() error {
+func oprDB() error {
 	// do some db opr
 	//  ...
+	rand.Seed(time.Now().Unix())
+	r := rand.Intn(2)
+	if r == 0 {
+		return nil
+	}
+
+	return sql.ErrNoRows
+}
+
+func innerDao(querySql string) error {
+	err := oprDB()
 
 	// 根因处使用Wrap，生成堆栈信息
-	err := errors.Wrap(sql.ErrNoRows, "fn1")
-	return err
+	if err == sql.ErrNoRows {
+		return errors.Wrap(NotFound, fmt.Sprintf("data  not found, sql[%s]", querySql))
+	}
+
+	if err != nil {
+		return errors.Wrap(err, fmt.Sprintf("db query system err, sql[%s]", querySql))
+	}
+
+	// do something
+	return nil
+}
+
+func Dao(querySql string) error {
+	//
+	err := innerDao(querySql)
+	if err != nil {
+		// 非根因处使用WithMessage，无需生成堆栈信息
+		return errors.WithMessage(err, "Dao")
+	}
+
+	return nil
 }
 
 func main() {
-	err := fn1()
+	querySql := "this is a executable sql"
+	err := Dao(querySql)
 	if err != nil {
 		fmt.Println(err)
+	} else {
+		fmt.Println("sql exec succ")
 	}
 	fmt.Println(SEPARATOR_LINE)
 
-	if errors.Is(err, sql.ErrNoRows) {
+	if errors.Is(err, NotFound) {
 		fmt.Printf("%+v\n", err)
 	}
 	fmt.Println(SEPARATOR_LINE)
 
-	if errors.Is(errors.Cause(err), sql.ErrNoRows) {
+	if errors.Is(errors.Cause(err), NotFound) {
 		fmt.Printf("%+v\n", err)
 	}
-	fmt.Println(SEPARATOR_LINE)
 
 	return
 }
